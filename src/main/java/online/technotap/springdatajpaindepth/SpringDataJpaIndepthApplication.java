dc7f176520fb4eb3934e1a3d06834ec7bc131374
shyam <strikerallin1@gmail.com>
2021-11-21 14:25:31 +0530

    Initial commit

---
+++ src/main/java/online/technotap/springdatajpaindepth/SpringDataJpaIndepthApplication.java
@@ -0,0 +1,13 @@
+package online.technotap.springdatajpaindepth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaIndepthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaIndepthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/technotap/springdatajpaindepth/SpringDataJpaIndepthApplicationTests.java
@@ -0,0 +1,13 @@
+package online.technotap.springdatajpaindepth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaIndepthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
